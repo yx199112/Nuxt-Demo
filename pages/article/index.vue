3bdede1e136e8bce54b5a960b8d379320d6428de
yx199112 <974435495@qq.com>
2021-05-01 21:58:52 +0800

    add

--- pages/article/index.vue
+++ pages/article/index.vue
@@ -1,18 +1,13 @@
 <template>
   <div class="article-page">
-
     <div class="banner">
       <div class="container">
-
         <h1>{{ article.title }}</h1>
-
         <article-meta :article="article" />
-
       </div>
     </div>
 
     <div class="container page">
-
       <div class="row article-content">
         <div class="col-md-12" v-html="article.body"></div>
       </div>
@@ -24,52 +19,48 @@
       </div>
 
       <div class="row">
-
         <div class="col-xs-12 col-md-8 offset-md-2">
-
           <article-comments :article="article" />
-
         </div>
-
       </div>
-
     </div>
-
   </div>
 </template>
 
 <script>
-import { getArticle } from '@/api/article'
-import MarkdownIt from 'markdown-it'
-import ArticleMeta from './components/article-meta'
-import ArticleComments from './components/article-comments'
+import { getArticle } from "@/api/article";
+import MarkdownIt from "markdown-it";
+import ArticleMeta from "./components/article-meta";
+import ArticleComments from "./components/article-comments";
 
 export default {
-  name: 'ArticleIndex',
-  async asyncData ({ params }) {
-    const { data } = await getArticle(params.slug)
-    const { article } = data
-    const md = new MarkdownIt()
-    article.body = md.render(article.body)
+  name: "ArticleIndex",
+  async asyncData({ params }) {
+    const { data } = await getArticle(params.slug);
+    const { article } = data;
+    const md = new MarkdownIt();
+    article.body = md.render(article.body);
     return {
-      article
-    }
+      article,
+    };
   },
   components: {
     ArticleMeta,
-    ArticleComments
+    ArticleComments,
   },
-  head () {
+  head() {
     return {
       title: `${this.article.title} - RealWorld`,
       meta: [
-        { hid: 'description', name: 'description', content: this.article.description }
-      ]
-    }
-  }
-}
+        {
+          hid: "description",
+          name: "description",
+          content: this.article.description,
+        },
+      ],
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
